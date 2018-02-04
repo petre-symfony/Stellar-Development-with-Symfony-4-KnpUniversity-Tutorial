929b677e1ea6b2b169eac98de3ef30948a6b99cb
petrero <petrero@gmail.com>
2018-02-04 12:26:47 +0200

    Now, about these annotation routes. Comment-out the YAML route.
    Then, in ArticleController, above the controller method, add /**, hit enter, clear this out, and say @Route(). You can use either class - but make sure PhpStorm adds the use statement on top. Then add '/'.
    
    That's it! The route is defined right above the controller, which is why I love annotation routes: everything is in one place. But don't trust me, find your browser and refresh. It's a traaaap! I mean, it works!
    
     Tip
    
    What exactly are annotations? They're PHP comments that are read as configuration.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,12 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ArticleController {
+  /**
+   * @Route("/")
+   */  
   public function homepage(){
     return new Response('OMG My first page already!');
   }
