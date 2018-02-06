d22959bd698a5cdb0e0ae7e0ec4a3492e34d943f
petrero <petrero@gmail.com>
2018-02-06 18:58:36 +0200

    Copy code from branch 11

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController {
+  /**
+   * @Route("/", name="app_homepage")
+   */  
+  public function homepage(){
+    return $this->render('article/homepage.html.twig');
+  }
+  
+  /**
+   * @Route("/news/{slug}", name="article_show")
+   */
+  public function show($slug) {
+    $comments = [
+      'I ate a normal rock once. It did NOT taste like bacon!',
+      'Woohoo! I\'m going on an all-asteroid diet!',
+      'I like bacon too! Buy some from my site! bakinsomebacon.com',
+    ];  
+    
+    return $this->render('article/show.html.twig', [
+      'title' => ucwords(str_replace('-', ' ', $slug)),
+      'comments' => $comments
+    ]);
+  }
+}
