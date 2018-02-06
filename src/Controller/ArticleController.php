8c38fbf7101a20d9ba03f024b9cb246990d19989
petrero <petrero@gmail.com>
2018-02-06 17:34:35 +0200

    Making the Homepage Pretty We now have a link to our homepage... but I don't know why you'd want to go here: it's super ugly! So let's render a template. In ArticleController, instead of returning a Response, return ->render() with article/homepage.html.twig.
    For now, don't pass any variables to the template.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,7 +10,7 @@ class ArticleController extends AbstractController {
    * @Route("/", name="app_homepage")
    */  
   public function homepage(){
-    return new Response('OMG My first page already!');
+    return $this->render('article/homepage.html.twig');
   }
   
   /**
